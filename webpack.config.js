99c3e7bef73e8837cf0707c7e6de219aa445fac2
y-golde <yon.golde@gmail.com>
2020-09-05 00:08:55 -0700

    changes the bundle reference

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     entry: "./src/index.js",
     output: {
         path: path.join(__dirname, "/public"),
-        filename: "bundle.js",
+        filename: "public/bundle.js",
     },
     module: {
         rules: [
