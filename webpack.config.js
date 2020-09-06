40cb536dd7fcac881811582760c4ffd2cdb494c2
y-golde <yon.golde@gmail.com>
2020-09-05 22:03:29 -0700

    changed typo in config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
     entry: "./src/index.js",
     output: {
-        path: path.join(__dirname, "/pubic"),
+        path: path.join(__dirname, "/public"),
         filename: "bundle.js",
         publicPath: "/",
     },
