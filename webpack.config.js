376a6a814b54f298f20d1b591d6950fb8b387ea3
y-golde <yon.golde@gmail.com>
2020-09-06 02:03:41 -0700

    try to add to devserver

--- webpack.config.js
+++ webpack.config.js
@@ -35,5 +35,10 @@ module.exports = {
             },
         ],
     },
+    devServer: {
+        historyApiFallback: true,
+        contentBase: "./",
+        hot: true,
+    },
     plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
 };
